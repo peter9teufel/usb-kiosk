01ec12edb8a74750ac7ebd310ebc2265f1a1cab8
Peter Neunteufel <peter9teufel@gmail.com>
2014-08-04 09:18:30 +0200

    Added auto resize of page text if text would overflow DIV in original size, removed unused css file.

--- html/infoscreen.js
+++ html/infoscreen.js
@@ -31,11 +31,41 @@ function switchPage(){
         textfield.innerHTML = infotext;
         image.src = img;
 
+        // auto size text to avoid overflow
+        autoSizeText()
+
         // increment pageNr and set timeout for next page switch
         pageNr++;
         setTimeout("switchPage()", 20000);
+    }
 }
-}
+
+function autoSizeText() {
+  var el, elements, _i, _len, _results;
+  elements = $('.resize');
+  console.log(elements);
+  if (elements.length < 0) {
+    return;
+  }
+  _results = [];
+  for (_i = 0, _len = elements.length; _i < _len; _i++) {
+    el = elements[_i];
+    _results.push((function(el) {
+      var resizeText, _results1;
+      resizeText = function() {
+        var elNewFontSize;
+        elNewFontSize = (parseInt($(el).css('font-size').slice(0, -2)) - 0.2) + 'em';
+        return $(el).css('font-size', elNewFontSize);
+      };
+      _results1 = [];
+      while (el.scrollHeight > el.offsetHeight) {
+        _results1.push(resizeText());
+      }
+      return _results1;
+    })(el));
+  }
+  return _results;
+};
 
 function httpGet(theUrl)
 {
