dd7b86d1a036d422f236ac65eb4833b5bb54ae05
root <root@raspberrypi.(none)>
2014-08-05 12:50:09 +0200

    Update player script fixed and tested on rpi.

--- update_player.sh
+++ update_player.sh
@@ -2,12 +2,13 @@
 
 cd /home/pi/usb-kiosk
 # check if network connected
-result=$(ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && echo ok || echo error)
-if [ "$result" == "ok" ]; then
+result=$(ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && echo ok || echo error);
+
+if [ "$result" = "ok" ]; then
         echo "Network connection available, trying to update sources...";
-        { # 'try' block
+        {
                 sudo git pull;
-        } || { # 'catch' block
+        } || {
                 echo "Updating sources failed, will try again on next reboot.";
         }
 fi
